3b0a9813ff64e9bb52fe8da8f57f36ececd0f36e
Hai Tang <402696415@qq.com>
2016-10-04 18:03:21 -0500

    process

--- training/ch1/1_7ProcessControl.c
+++ training/ch1/1_7ProcessControl.c
@@ -1,4 +1,4 @@
-#include "apue.h"
+#include <stdio.h>
 #include <sys/wait.h>
 
 #define MAXLINE 100
@@ -11,6 +11,21 @@ int main(void)
 
 	printf("%% ");
 	while(fgets(buf,MAXLINE,stdin)!=NULL) {
-	
+		if(buf[strlen(buf)-1] == '\n')
+			buf[strlen(buf)-1] = 0;		/*replace new line with null*/
+
+		if((pid=fork())<0)
+			printf("fork error!\n");
+		else if(pid == 0) {
+			execlp(buf,buf,(char *)0);	/* google it */
+			printf("Could't execute:%s",buf);
+			exit(127);
+		}
+
+		/* parent */
+		if((pid = waitpid(pid,&status,0)) < 0)
+			printf("waitpid error\n");
+		printf("%% ");
 	}
+	exit(0);
 }
